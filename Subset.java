05853ac278e89309fccbc74c184d351af867e776
Jakub Sikora <jakub.sikora.en@gmail.com>
2015-02-11 22:09:15 +0000

    1st submit

---
+++ Subset.java
@@ -0,0 +1,20 @@
+import java.util.Iterator;
+
+public class Subset {
+  public static void main(String[] args) {
+    int k = Integer.parseInt(args[0]);
+    
+    RandomizedQueue<String> s = new RandomizedQueue<String>();
+    
+    while (!StdIn.isEmpty()) {
+      String item = StdIn.readString();
+      if (!item.equals(" ")) s.enqueue(item);
+    }
+    
+    Iterator itr = s.iterator();
+    while(itr.hasNext() && k > 0) {
+      StdOut.println(s.dequeue());
+      k--;
+    }
+  }
+}
