7d1e44c1476d879f81848ce5d85f6d493ac065ff
Jakub Sikora <jakub.sikora@pure360.com>
2015-02-11 14:41:56 +0000

    random queue

--- RandomizedQueue.java
+++ RandomizedQueue.java
@@ -1,10 +1,13 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 public class RandomizedQueue<Item> implements Iterable<Item> {
   private Item[] a;         // array of items
   private int N;            // number of elements on stack
-  
+
   // construct an empty randomized queue
   public RandomizedQueue() {
-    a = (Item[]) new Object[2];
+    a = (Item[]) new Object[1];
   }
 
   // is the queue empty?
@@ -16,44 +19,91 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
   public int size() {
     return N;
   }
-  
+
   private void resize(int capacity) {
     Item[] temp = (Item[]) new Object[capacity];
-    
+
     for (int i = 0; i < N; i++) {
       temp[i] = a[i];
     }
-    
+
+    a = temp;
+  }
+
+  private void shuffle() {
+    Item[] temp = (Item[]) new Object[N];
+    int j = 0;
+
+    for (int i = 0; i < N; i++) {
+      if (a[i]) {
+        temp[j] = a[i];
+        j++;
+      }
+    }
+
     a = temp;
   }
 
   // add the item
   public void enqueue(Item item) {
+    if (item == null) throw new NullPointerException();
+
     if (N == a.length) resize(2*a.length);    // double size of array if necessary
     a[N++] = item;
   }
 
   // remove and return a random item
   public Item dequeue() {
+    if (size() == 0) throw new NoSuchElementException();
+
     int rand = StdRandom.uniform(N);
-    
+
     Item item = a[rand];
-    a[rand] = null;                              // to avoid loitering
+    a[rand] = null;
     N--;
-    
+
+    // shuffle array
+    shuffle();
+
     // shrink size of array if necessary
     if (N > 0 && N == a.length/4) resize(a.length/2);
-    return item; 
+    return item;
   }
 
   // return (but do not remove) a random item
   public Item sample() {
+    if (size() == 0) throw new NoSuchElementException();
 
+    int rand = StdRandom.uniform(N);
+    Item item = a[rand];
+    return item;
   }
 
   // return an independent iterator over items in random order
   public Iterator<Item> iterator() {
+    return new ReverseArrayIterator();
+  }
 
+  // an iterator, doesn't implement remove() since it's optional
+  private class ReverseArrayIterator implements Iterator<Item> {
+    private int i;
+
+    public ReverseArrayIterator() {
+      i = N;
+    }
+
+    public boolean hasNext() {
+      return i > 0;
+    }
+
+    public void remove() {
+      throw new UnsupportedOperationException();
+    }
+
+    public Item next() {
+      if (!hasNext()) throw new NoSuchElementException();
+      return a[--i];
+    }
   }
 
   // unit testing
